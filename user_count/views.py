cf7351ec8363c2866faac8bfb0b240ad59d6fe0c
chtw2001 <jungtaekwon1019@gmail.com>
2023-08-17 10:26:00 +0900

    [create] user_count module

---
+++ user_count/views.py
@@ -0,0 +1,30 @@
+from .models import Counter
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.http import HttpResponse, HttpResponseRedirect
+from datetime import datetime, timedelta
+
+
+# just use this module for user count
+def user_count(request):
+
+	try:
+		user = Counter.objects.get(date=timezone.now())
+	except Counter.DoesNotExist:
+		user = Counter.objects.create(count=0, date=timezone.now())
+
+	if request.COOKIES.get('visited') == 'yes':
+		return redirect('main')  # have to naming 'main' page , ex) path('/index', views.hi, name='main)
+	else:
+		key = 'visited'
+		value = 'yes'
+		now = datetime.now()
+		end_of_day = (now + timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
+		expire = end_of_day.strftime('%a, %d %b %Y %H:%M:%S GMT')
+
+		res = HttpResponseRedirect('main')
+		res.set_cookie(key, value=value, expires=expire)
+		user.count += 1
+		user.save()
+  
+		return res
