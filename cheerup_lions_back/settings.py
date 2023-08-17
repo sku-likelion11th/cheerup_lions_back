fdfbc2ce441af17f38e39c7a2b7ae3def69f9053
Jym-lab <jym9809@gmail.com>
2023-08-17 11:37:08 +0900

    feat: hide settings.py

--- cheerup_lions_back/settings.py
+++ cheerup_lions_back/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from . import local_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4!j%-q0t((op-s10r=#j22#=tgh2bojnt9=w)&rbio%iv74$r^'
+SECRET_KEY = local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,12 +78,7 @@ WSGI_APPLICATION = 'cheerup_lions_back.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = local_settings.DATABASES
 
 
 # Password validation
