52e1d03593f190b1c73b16ca51d679c6666569c0
seung-heee <tmdgmldi2595@naver.com>
2023-08-18 20:02:07 +0900

    가라 text 삭제

--- cheerup_board/static/js/text-typing.js
+++ cheerup_board/static/js/text-typing.js
@@ -1,4 +1,4 @@
-var words = ['해커톤에 참여한 여러분, 파이팅!', '해커톤에서 모두의 열정을 보여줘!', '해커톤 참가자 모두 힘냅시다!', '지금의 노력이 모두 성공으로 이어질 거예요.', '해커톤에서 놀라운 결과물을 만들어봐요!', '함께 힘내서 이번 해커톤도 대성공하자!'];
+var words = [];
 var part, i = 0, offset = 0, len = words.length, forwards = true, skip_count = 0, skip_delay = 15, speed = 70;
 
 var wordflick = function () {
@@ -47,6 +47,6 @@ function fetchAndAddWords() {
 }
 
 $(document).ready(function () {
-    wordflick();
     fetchAndAddWords();
+    wordflick();
 });
