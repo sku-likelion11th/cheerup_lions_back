3884da738a8dc6918dc9cc8ee5a22c1d0ed630fe
seung-heee <tmdgmldi2595@naver.com>
2023-08-18 20:35:42 +0900

    yeah~~

--- cheerup_board/static/js/main_background.js
+++ cheerup_board/static/js/main_background.js
@@ -1,29 +1,30 @@
-const images = [
-    '/static/img/start2.jpg',
-    '/static/img/start.jpg',
-  ];
-  
 const backgroundImage = document.querySelector('.background-image');
 let currentImageIndex = 0;
+let fetchedImages = [];
 
 fetch('/get_images/')
 .then(response => response.json())
 .then(data => {
-  const fetchedImage = data.images.map(url => `media/${url}`);
-  images.push(...fetchedImage);
+  fetchedImages = data.images.map(url => `media/${url}`);
+  if (fetchedImages.length > 0) {
+    changeBackgroundImage(); // Start changing background images
+  }
 })
 
 function changeBackgroundImage() {
+  if (fetchedImages.length === 0) {
+    return; // No images to display
+  }
 
-  backgroundImage.style.backgroundImage = `url('${images[currentImageIndex]}')`;
+  const imageUrl = fetchedImages[currentImageIndex];
+
+  backgroundImage.style.backgroundImage = `url('${imageUrl}')`;
   backgroundImage.style.opacity = 1;
-  
+
   setTimeout(() => {
     backgroundImage.style.opacity = 0;
   }, 3000);
 
-  currentImageIndex = (currentImageIndex + 1) % images.length;
+  currentImageIndex = (currentImageIndex + 1) % fetchedImages.length;
   setTimeout(changeBackgroundImage, 4000);
 }
-
-changeBackgroundImage();
